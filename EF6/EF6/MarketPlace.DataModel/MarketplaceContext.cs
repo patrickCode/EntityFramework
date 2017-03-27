10c5d597dcc200c26fc1fefa1ef6cf03eb0e0642
Pratik Bhattacharya <pratikb@microsoft.com>
2017-03-27 21:26:50 +0530

    Seed Code

---
+++ EF6/EF6/MarketPlace.DataModel/MarketplaceContext.cs
@@ -0,0 +1,15 @@
+﻿using MartetPlace.Obj;
+using System.Data.Entity;
+
+namespace MarketPlace.DataModel
+{
+    public class MarketplaceContext: DbContext
+    {
+        //DbSet refers to a repository
+        public DbSet<Shipper> Shippers { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<PickupLocation> PickupLocations { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
